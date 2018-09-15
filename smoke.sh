79fd889d28b06ed8cb630af5c532db9586dfa370
dsalamancaMS <desalama@microsoft.com>
2018-09-15 16:30:14 -0600

    Functions added on first tests

--- smoke.sh
+++ smoke.sh
@@ -7,16 +7,13 @@ BLUE='\033[1;34m'
 PRPL='\033[1;35m'
 
 > results.txt
- echo -e '-------------------- Testing Kube Components --------------------'
-
 
 date >> results.txt
 
-echo -e "\n--------Test #1 - Namespace creation--------" 
+#echo -e "\n--------Test #1 - Namespace creation--------" 
 
-echo -e "\n >>>>>>>>>> Creating Namespace"
 
-function namespace_create(){
+function t1_namespc_crt(){
 
 kubectl create namespace testspace 
 
@@ -24,91 +21,99 @@ kubectl create namespace testspace
 
 # echo -e "\n--------Test #2 - Pod & image Pull--------" 
 
-# echo -e '\n >>>>>>>>>> Pulling Image and Creating Pod'
 
-# cat << EOF | kubectl create -f - 
-# apiVersion: v1
-# kind: Pod
-# metadata:
-#   namespace: testspace
-#   name: image-pull-test
-# spec:
-#   restartPolicy: Never
-#   containers:
-#   - name: image-test
-#     image: busybox
-#     imagePullPolicy: Always
-#     args:
-#     - echo
-#     - "success" 
-# EOF
-
+function t2_img_pull(){
+cat << EOF | kubectl create -f - 
+apiVersion: v1
+kind: Pod
+metadata:
+  namespace: testspace
+  name: image-pull-test
+spec:
+  restartPolicy: Never
+  containers:
+  - name: image-test
+    image: busybox
+    imagePullPolicy: Always
+    args:
+    - echo
+    - "success" 
+EOF
+}
 
 # echo -e "\n--------Test #3 - Communication Between Pods--------" 
 
-# echo -e "\n >>>>>>>>>> Creating Pods for Test"
 
-# #remember to change sleep
-# cat << EOF | kubectl create -f - 
-# apiVersion: apps/v1
-# kind: DaemonSet
-# metadata:
-#   namespace: testspace
-#   name: ping-daemons
-#   labels:
-#     ping: test
-# spec:
-#   selector:
-#     matchLabels:
-#       ping: pods
-#   template:
-#     metadata:
-#       labels:
-#         ping: pods
-#     spec:
-#       containers:
-#       - name: ping-pod
-#         image: busybox:1.27
-#         args:
-#         - sleep
-#         - "10000"
-# EOF
+
+function t3_demon_constr(){
+ 
+ cat << EOF | kubectl create -f - 
+apiVersion: apps/v1
+kind: DaemonSet
+metadata:
+  namespace: testspace
+  name: ping-daemons
+  labels:
+    ping: test
+spec:
+  selector:
+    matchLabels:
+      ping: pods
+  template:
+    metadata:
+      labels:
+        ping: pods
+    spec:
+      containers:
+      - name: ping-pod
+        image: busybox:1.27
+        args:
+        - sleep
+        - "10000"
+EOF
+}
 
 
 # sleep 60
 
 ######## Remember to put Timer
 
-function get_ping_ips() {
+function t3_get_ping_ips() {
 
 kubectl get po -n testspace -l ping=pods -o json | grep -i "podIP" | awk '{print $2}' | sed 's/[",]//g'
 
 }
 
-function get_ping_pods() {
+function t3_get_ping_pods() {
 
 kubectl get pods -n testspace | grep ping-daemons | awk '{print $1}' 
 
 }
 
+function t3_pint_test(){
+  
+  echo -e "\n>>>>Testing connectivity from $(get_ping_pods | head -1) to $(get_ping_pods | tail -1)\n" 
 
-# echo -e "\n>>>>Testing connectivity from $(get_ping_pods | head -1) to $(get_ping_pods | tail -1)\n" 
+  kubectl exec -n testspace $(get_ping_pods | head -1) -it -- ping -c 4 $(get_ping_ips | tail -1)  
 
-# kubectl exec -n testspace $(get_ping_pods | head -1) -it -- ping -c 4 $(get_ping_ips | tail -1)  
+  echo -e "\n>>>>Testing connectivity from $(get_ping_pods | head -1) to $(get_ping_pods | tail -1)\n" 
 
-# echo -e "\n>>>>Testing connectivity from $(get_ping_pods | head -1) to $(get_ping_pods | tail -1)\n" 
+  kubectl exec -n testspace $(get_ping_pods | tail -1) -it -- ping -c 4 $(get_ping_ips | head -1) 
 
-# kubectl exec -n testspace $(get_ping_pods | tail -1) -it -- ping -c 4 $(get_ping_ips | head -1) 
+}
+
 
 # echo -e "\n--------Test #4 - Public DNS Lookup--------" 
 
-# for i in $(get_ping_pods)
-# do
-#     echo -e "\n>>>>Testing Public DNS lookup on $i\n"
-#     kubectl exec -n testspace $i -- nslookup google.com
-# done
+function t4_dns_tst(){
 
+  for i in $(get_ping_pods)
+  do
+     echo -e "\n>>>>Testing Public DNS lookup on $i\n"
+     kubectl exec -n testspace $i -- nslookup google.com
+  done
 
+}
 
 #Test 5 Deployment functions-----------------------------------------------------------------------------------------------------------------------------------------------   
 
@@ -386,6 +391,9 @@ spec:
   containers:
     - name: pvc-cont
       image: alpine
+      args:
+      - sleep
+      - "1000"
       volumeMounts:
       - mountPath: "/mnt/azure"
         name: volume
@@ -394,6 +402,10 @@ spec:
       persistentVolumeClaim:
         claimName: test-pvc
 EOF
+
+echo -e "Waiting for Pod to be Created. This might take around 15 sec or more"
+
+sleep 20
 
 }
 
