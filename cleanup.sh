dd0b8e8e32af0e294e472b999d044333d40fb6b3
dsalamancaMS <desalama@microsoft.com>
2018-09-15 16:34:08 -0600

    Cleanup updated

--- cleanup.sh
+++ cleanup.sh
@@ -7,5 +7,7 @@ kubectl delete deploy -n testspace deploy-test
 kubectl delete svc -n testspace test-service
 kubectl delete deploy -n testspace svc-deploy
 kubectl delete pod -n testspace curl-pod
-
+kubectl delete pod -n testspace pvc-pod
+kubectl delete pvc -n testspace
+kubectl delete pv -n testspace
 #kubectl delete namespace testspace
